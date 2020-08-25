91b6718054db358527a712c52d9458bd24267d1d
tsaduranga <tsaduranga@gmail.com>
2020-08-25 17:51:48 +0530

    modify payment views

--- resources/views/backend/payment-gateway/show.blade.php
+++ resources/views/backend/payment-gateway/show.blade.php
@@ -12,113 +12,58 @@
 
 
           
-            <form class="form-horizontal" >
+            
 
-              <div class="form-row" >
+            <table class="table table-striped table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">##</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>payment ID</td>
+                  <td>{{ $payment_gateway['id'] }}</td>
+                </tr>
+                <tr>
+                  <td>Payment Date</td>
+                  <td>{{ $payment_gateway['created_at'] }}</td>
+                </tr>
+                <tr>
+                  <td>Payment Amount</td>
+                  <td>{{ $payment_gateway['payment_amount']  }}</td>
+                </tr>
+                <tr>
+                  <td>Payment bank</td>
+                  <td>{{  $payment_gateway['payment_bank'] }}</td>
+                </tr>
+                <tr>
+                  <td>customer ID</td>
+                  <td>{{ $payment_gateway->BidUser->id  }}</td>
+                </tr>
 
-                <div class="form-group col-md-6">
-                  <label for="inputEmail4">Payment ID</label>
-                  <input type="text" class="form-control" id="package_name" name="package_name" value="{{ $payment_gateway['id'] }}" disabled>
-                  
-              </div>
-
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">reciepts code</label>
-                <input type="text" class="form-control" id="package_price" value="" name="package_price" disabled>
-
-              </div>
-
-              </div>
+                <tr>
+                  <td>Customer Name</td>
+                  <td>{{ $payment_gateway->BidUser->user_fname }}</td>
+                </tr>
 
 
-              <div class="form-row" >            
-                <div class="form-group col-md-6">
-                  <label for="inputPassword4">Payment Date</label>
-                  <input type="text" class="form-control" id="package_name" name="package_name" value="{{ $payment_gateway['created_at'] }}" disabled>
-                </div>
+                <tr>
+                  <td>Selected Package Name</td>
+                  <td>{{  $payment_gateway->package->package_name }}</td>
+                </tr>
+
+                <tr>
+                  <td>Selected Package Name</td>
+                  <td>{{ $payment_gateway->package->package_price }} USD</td>
+                </tr>
+
                 
 
-                <div class="form-group col-md-6">
-                  <label for="inputEmail4">Payment Amount</label>
-                  <input type="number" class="form-control" id="package_price" value="{{ $payment_gateway['payment_amount']  }}" name="package_price" disabled>
-  
-                </div>
 
-              </div>
-
-
-
-
-              <div class="form-row">
-
-                  
-
-                  <div class="form-group col-md-6">
-                    <label for="inputPassword4">Payment bank</label>
-                    <input type="text" name="package_rolls" class="form-control"  value="{{  $payment_gateway['payment_bank'] }}" id="package_rolls" disabled>
-                  
-                  </div>
-                </div>
-
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputEmail4">customer ID</label>
-                    <input type="number" class="form-control" id="package_price" value="{{ $payment_gateway->BidUser->id  }}" name="package_price" disabled>
-    
-                  </div>
-
-                    <div class="form-group col-md-6">
-                      <label for="inputPassword4">Customer Name</label>
-                      <input type="text" name="create_user" class="form-control"  value="{{ $payment_gateway->BidUser->user_fname }}" id="package_rolls" disabled>
-                    
-                    </div>
-
-                </div>
-
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputEmail4">package ID</label>
-                    <input type="number" class="form-control" id="package_price" value="{{ $payment_gateway->package->id  }}" name="package_price" disabled>
-    
-                  </div>
-
-                    <div class="form-group col-md-6">
-                      <label for="inputPassword4">package Name</label>
-                      <input type="text" name="create_user" class="form-control"  value="{{  $payment_gateway->package->package_name }}" id="package_rolls" disabled>
-                    
-                    </div>
-
-                </div>
-
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputEmail4">package price</label>
-                    <input type="number" class="form-control" id="package_price" value="{{ $payment_gateway->package->package_price }}" name="package_price" disabled>
-    
-                  </div>
-
-                    <div class="form-group col-md-6">
-                      <label for="inputPassword4">package Bids</label>
-                      <input type="text" name="create_user" class="form-control"  value="{{ $payment_gateway->package->package_rolls }}" id="package_rolls" disabled>
-                    
-                    </div>
-
-                </div>
-
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputEmail4">reciepts code</label>
-                    <input type="number" class="form-control" id="package_price" value="" name="package_price" disabled>
-    
-                  </div>
-
-                  
-
-                </div>
-
-
-
-            </form>
+              </tbody>
+            </table>
 
 
                 
